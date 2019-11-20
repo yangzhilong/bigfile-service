5e393ee5d02870fdbd5e5016413e7e9b57925b6c
roger yang <roger.yang@nike.com>
2019-11-20 10:06:51 +0800

    update flux test case

--- src/test/java/com/longge/FluxTest.java
+++ src/test/java/com/longge/FluxTest.java
@@ -49,10 +49,10 @@ public class FluxTest {
                     latch.countDown();
                 }
             })
+            .take(5)
             .doOnComplete(() -> latch.countDown())
             .subscribe(System.err::println);
         latch.await();
-        
         
         System.out.println("------------");
         
@@ -138,5 +138,50 @@ public class FluxTest {
         System.out.println("------------");
         
         Flux.range(1, 5).zipWith(Flux.just(6, 7, 8)).zipWithIterable(Arrays.asList(9, 10, 11, 12)).subscribe(System.out::println);
+    }
+    
+    @Test
+    public void testTake() throws Exception {
+        Flux.range(1, 10).take(3).subscribe(System.out::println);
+        
+        Thread.sleep(500L);
+        System.out.println("--------------");
+        
+        Flux.range(1, 10).takeLast(3).subscribe(System.err::println);
+        
+        Thread.sleep(500L);
+        System.out.println("--------------");
+        
+        // 条件为true时获取，如果一开始为false则一个都不取
+        Flux.range(1, 10).takeWhile(c -> c >= 1 && c < 5).subscribe(System.out::println);
+        
+        Thread.sleep(500L);
+        System.out.println("--------------");
+        
+        // 先取元素，直接条件为true时停止
+        Flux.range(1, 10).takeUntil(c -> c > 1 && c < 5).subscribe(System.err::println);
+        
+        Thread.sleep(500L);
+        System.out.println("--------------");
+        
+        // 则是先取元素，直到别一个Flux序列产生元素
+        Flux.range(1, 4).takeUntilOther(Flux.never()).subscribe(System.out::println);
+    }
+    
+    @Test
+    public void testCombine() {
+        Flux.combineLatest(
+            Arrays::toString,
+            Flux.just(0, 1),
+            Flux.just("A", "B"))
+            .toStream().forEach(System.out::println);
+        
+        System.out.println("--------------");
+        
+        Flux.first(Flux.just(1,2,3)).subscribe(System.out::println);
+        
+        System.out.println("--------------");
+        
+        Flux.first(Flux.just(1,2,3), Flux.just(4,5)).subscribe(System.out::println);
     }
 }
